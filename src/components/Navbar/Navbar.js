cb0e4df34ac9ef30cbe5df99a290722a31a4b03b
Michal <michal@gmail.com>
2017-09-11 20:30:38 +0200

    responsive navigation fully working on almost every phone

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -21,12 +21,22 @@ export default class Navbar extends Component {
                     <div className={ this.state.toggleMenu ? "toggle__hamburger toggle__hamburger--toggled": "toggle__hamburger"} onClick={ this.toggleMenu.bind( this ) }>
                     </div>
                 </div>
-                <ul className={ this.state.toggleMenu ? "nav__menu nav__menu--open" : "nav__menu nav__menu--closed"}>
-                    <li className="menu__item fa fa-home"/>
-                    <li className="menu__item fa fa-github"/>
-                    <li className="menu__item fa fa-codepen"/>
-                    <li className="menu__item fa fa-gamepad"/>
-                    <li className="menu__item fa fa-linkedin"/>
+                 <ul className={ this.state.toggleMenu ? "nav__menu nav__menu--open" : "nav__menu nav__menu--closed"}>
+                     <li className="menu__item">
+                         <span className="fa fa-home" />
+                     </li>
+                     <li className="menu__item">
+                         <span className="fa fa-github" />
+                     </li>
+                     <li className="menu__item">
+                         <span className="fa fa-codepen" />
+                     </li>
+                     <li className="menu__item">
+                         <span className="fa fa-gamepad" />
+                     </li>
+                     <li className="menu__item">
+                         <span className="fa fa-linkedin" />
+                     </li>
                 </ul>
             </div>
         );
