8c911f4885e8c659ac011610028201e32ec6b1ec
Michal <michal@gmail.com>
2017-09-12 08:31:00 +0200

    Coded navbar on desktop, added some fireworks and changing colors

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,11 +6,11 @@ class Landing extends Component {
         return(
             <div className="landing">
                 <h1 className="landing__title">michal<strong>dev</strong></h1>
-                <MediaQuery className="centered" query='(max-width: 798px)'>
+                <MediaQuery className="centered" query='(max-width: 769px)'>
                     <span className="landing__subtitle">Learn more</span>
                     <div className="landing__arrow" />
                 </MediaQuery>
-                <MediaQuery query='(min-width: 799px)'>
+                <MediaQuery query='(min-width: 770px)'>
                     <div className="landing__button landing__button--close">More</div>
                 </MediaQuery>
             </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MediaQuery from 'react-responsive';
 
 export default class Navbar extends Component {
     constructor( props ) {
@@ -16,28 +17,58 @@ export default class Navbar extends Component {
 
     render() {
         return(
-            <div className="nav">
-                <div className="nav__toggle">
-                    <div className={ this.state.toggleMenu ? "toggle__hamburger toggle__hamburger--toggled": "toggle__hamburger"} onClick={ this.toggleMenu.bind( this ) }>
+            <div className="navbar">
+                <MediaQuery query='( max-width: 769px )'>
+                    <div className="nav">
+                        <div className="nav__toggle" onClick={ this.toggleMenu.bind( this ) }>
+                            <div className={ this.state.toggleMenu ? "toggle__hamburger toggle__hamburger--toggled": "toggle__hamburger"}>
+                            </div>
+                        </div>
+                        <ul className={ this.state.toggleMenu ? "nav__menu nav__menu--open" : "nav__menu nav__menu--closed"}>
+                            <li className="menu__item">
+                                <span className="fa fa-home" />
+                            </li>
+                            <li className="menu__item">
+                                <span className="fa fa-github" />
+                            </li>
+                            <li className="menu__item">
+                                <span className="fa fa-codepen" />
+                            </li>
+                            <li className="menu__item">
+                                <span className="fa fa-gamepad" />
+                            </li>
+                            <li className="menu__item">
+                                <span className="fa fa-linkedin" />
+                            </li>
+                        </ul>
                     </div>
-                </div>
-                 <ul className={ this.state.toggleMenu ? "nav__menu nav__menu--open" : "nav__menu nav__menu--closed"}>
-                     <li className="menu__item">
-                         <span className="fa fa-home" />
-                     </li>
-                     <li className="menu__item">
-                         <span className="fa fa-github" />
-                     </li>
-                     <li className="menu__item">
-                         <span className="fa fa-codepen" />
-                     </li>
-                     <li className="menu__item">
-                         <span className="fa fa-gamepad" />
-                     </li>
-                     <li className="menu__item">
-                         <span className="fa fa-linkedin" />
-                     </li>
-                </ul>
+                </MediaQuery>
+                <MediaQuery query='( min-width: 770px )'>
+                        <div className="nav--desktop">
+                            <div className="nav__home--desktop">
+                                <i className="fa fa-home" />
+                            </div>
+                            <div className="nav__logo--desktop">
+                                <h1> MichalDev </h1>
+                            </div>
+                            <div className="nav__menu--desktop">
+                                <div className="menu__icons--desktop">
+                                    <li className="menu__item--desktop">
+                                        <i className="fa fa-github" />
+                                    </li>
+                                    <li className="menu__item--desktop">
+                                        <i className="fa fa-codepen" />
+                                    </li>
+                                    <li className="menu__item--desktop">
+                                        <i className="fa fa-gamepad" />
+                                    </li>
+                                    <li className="menu__item--desktop">
+                                        <i className="fa fa-linkedin" />
+                                    </li>
+                                </div>
+                            </div>
+                        </div>
+                </MediaQuery>
             </div>
         );
     }
